3f560bf9d7a1debd9dbee057620e5db101debbdc
JaejinLee <ppaimar16@gmail.com>
2021-01-02 10:44:53 +0900

    [React]siteMapmodify career part not complite

--- src/components/SiteMapSection/Hire.js
+++ src/components/SiteMapSection/Hire.js
@@ -1,86 +1,139 @@
 import React, { Component } from 'react';
 import './Hire.css';
+import sgjg from '../../images/sgjg.png'
 
 class Hire extends Component {
     render() {
         return (
             <div className="hr_main">
-                <div id="hr_header" className="hr_header">
-                    <h1>SHIGO JAGO Careers</h1>
-                    <div className="gnb_wrap">
-                        <nav id="gnb" className="gnb">
-                            <ul className="dep1">
-                                <li>People</li>
-                                <li>Culture</li>
-                                <li>Jobs</li>
-                                <li>FAQs</li>
-                            </ul>
-                        </nav>
-                        <div className="language_list"><button id="header-languages" type="button" className="btn">KR</button>
-                            <div className="list">
-                                <ul>
-                                    <li><a href="#" className="active">KR</a></li>
-                                    <li><a href="#" className="">EN</a></li>
-                                </ul>
-                            </div>
+                <div className="hr_wrapper">
+                    <div className="hr_header clearfix">
+                        <div className="hr_logo">
+                            <h1>Find Your Next Career<span>Jobs</span></h1>
                         </div>
-                    </div><button type="button" className="btn_header_search"><span className="blind">Search</span></button>
-
-
-                </div>
-                <div>
-                    <div className="main_top">
-                            <div className="text_area">
-                                <p className="text"><span>One step closer</span><span>to a better future</span></p>
-                                <div className="select_option">
-                                    <div className="combo_box combo_location"><button id="location-dropdown" type="button" className="btn_combo_box">Location</button>
-                                        <ul className="combo_list">
-                                            <li><button type="button">Korea</button></li>
-                                            <li><button type="button">China</button></li>
-                                            <li><button type="button">Hong Kong</button></li>
-                                            <li><button type="button">Indonesia</button></li>
-                                            <li><button type="button">Japan</button></li>
-                                            <li><button type="button">Taiwan</button></li>
-                                            <li><button type="button">Thailand</button></li>
-                                            <li><button type="button">USA</button></li>
-                                        </ul><select className="main_select">
-                                            <option value="Location">Location</option>
-                                            <option value="Korea">Korea</option>
-                                            <option value="China">China</option>
-                                            <option value="Hong Kong">Hong Kong</option>
-                                            <option value="Indonesia">Indonesia</option>
-                                            <option value="Japan">Japan</option>
-                                            <option value="Taiwan">Taiwan</option>
-                                            <option value="Thailand">Thailand</option>
-                                            <option value="USA">USA</option>
-                                        </select>
-                                    </div>
-                                    <div className="combo_box combo_category"><button id="category-dropdown" type="button" class="btn_combo_box">Job Unit</button>
-                                        <ul className="combo_list">
-                                            <li><button type="button">Engineering</button></li>
-                                            <li><button type="button">Design</button></li>
-                                            <li><button type="button">Product Planning</button></li>
-                                            <li><button type="button">Business</button></li>
-                                            <li><button type="button">Management Support</button></li>
-                                        </ul><select className="main_select">
-                                            <option value="Job Unit">Job Unit</option>
-                                            <option value="Engineering">Engineering</option>
-                                            <option value="Design">Design</option>
-                                            <option value="Product Planning">Product Planning</option>
-                                            <option value="Business">Business</option>
-                                            <option value="Management Support">Management Support</option>
-                                        </select>
-                                    </div>
-                                </div><a className="btn_go disabled" hreflang="ko" href="/ko ">Search</a>
-                            </div>
-                            <div className="desc_scroll">Scroll</div>
-                        </div>
+                        <ul className="hr_nav">
+                            <li className="current">All Jobs</li>
+                            <li>Full-Time</li>
+                            <li>Part-Time</li>
+                            <li>Freelancer</li>
+                            <li>Contract</li>
+                        </ul>
                     </div>
 
+                    <ul className="timeline">
+                        <ul>
+                            <li className="listing clearfix">
+                                <div className="image_wrapper">
+                                    <img src={sgjg} alt='sgjg' />
+                                </div>
+                                <div className="job_full">
+                                    <span className="job_title">UX Designer</span>
+                                    <span className="job_info">ShigoJago<span>&bull;</span> Pangyo <span>&bull;</span> Jan 1st</span>
+                                </div>
+                                <span className="job_type part_time">Part-Time</span>
+
+
+                            </li>
+
+                        </ul>
+
+                        <ul>
+                            <li className="listing clearfix">
+                                <div className="image_wrapper">
+                                    <img src={sgjg} alt='sgjg' />
+                                </div>
+                                <div className="job_full">
+                                    <span className="job_title">Data Scientist</span>
+                                    <span className="job_info">ShigoJago<span>&bull;</span> Pangyo <span>&bull;</span> Jan 1st</span>
+                                </div>
+                                <span className="job_type full_time">Full-Time</span>
+
+
+                            </li>
+
+                        </ul>
+
+                        <ul>
+                            <li className="listing clearfix">
+                                <div className="image_wrapper">
+                                    <img src={sgjg} alt='Star' />
+                                </div>
+                                <div className="job_full">
+                                    <span className="job_title">Frontend Engineer</span>
+                                    <span className="job_info">ShigoJago<span>&bull;</span> Pangyo <span>&bull;</span> Jan 1st</span>
+                                </div>
+                                <span className="job_type freelance">Freelance</span>
+
+
+                            </li>
+
+                        </ul>
+
+                        <ul>
+                            <li className="listing clearfix">
+                                <div className="image_wrapper">
+                                    <img src={sgjg} alt='Star' />
+                                </div>
+                                <div className="job_full">
+                                    <span className="job_title">Backend Engineer</span>
+                                    <span className="job_info">ShigoJago<span>&bull;</span> Pangyo <span>&bull;</span> Jan 1st</span>
+                                </div>
+                                <span className="job_type full_time">Full-Time</span>
+
+
+                            </li>
+
+                        </ul>
+
+                        <li className="job_date">Jan 1st</li>
+
+                        <ul>
+                            <li className="listing clearfix">
+                                <div className="image_wrapper">
+                                    <img src={sgjg} alt='Star' />
+                                </div>
+                                <div className="job_full">
+                                    <span className="job_title">Product Manager</span>
+                                    <span className="job_info">ShigoJago<span>&bull;</span> Pangyo <span>&bull;</span> Dec 31st</span>
+                                </div>
+                                <span className="job_type contract">contract</span>
+
+
+                            </li>
+
+                        </ul>
+
+                        <ul>
+                            <li className="listing clearfix">
+                                <div className="image_wrapper">
+                                    <img src={sgjg} alt='Star' />
+                                </div>
+                                <div className="job_full">
+                                    <span className="job_title">Systems Engineer</span>
+                                    <span className="job_info">ShigoJago<span>&bull;</span> Pangyo <span>&bull;</span> Dec 31st</span>
+                                </div>
+                                <span className="job_type full_time">Full-Time</span>
+
+
+                            </li>
+
+                        </ul>
+
+                        <li className="job_date">Dec 31st</li>
+
+                        <li className="load_more">
+                            <button className="load_button">Load More</button>
+                        </li>
+                    </ul>
+
+
 
                 </div>
+            </div>
+
+
         )
-    
+
     }
 }
 
