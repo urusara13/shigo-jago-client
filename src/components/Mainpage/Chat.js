5d3b456de6637e864602385231b1b72ca922047c
urusara13 <urusara13@gmail.com>
2021-01-04 14:28:42 +0900

    fix

--- src/components/Mainpage/Chat.js
+++ src/components/Mainpage/Chat.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './Chat.css'
 export default function Chat ({isChat}) {
   return isChat && (
-    <div className="chatDiv">
-      <object aria-labelledby="label1" data="http://localhost:3080/set" width="400px" height="600px" style={{"float": "right", "position":"relative", "top":"400px", "left": "10px", "overflow":"hidden" }}></object>
+<div className="chatDiv">
+      <object aria-labelledby="label1" data="http://localhost:3080/set" width="510px" height="830px" style={{ "position":"fixed", "top":"200px", "left": "30px" }}></object>
     </div>
   )  
 }
