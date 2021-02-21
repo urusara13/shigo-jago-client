a5de725ec8d7b6722fe5423b20e31cff661308e3
urusara13 <urusara13@gmail.com>
2021-02-21 09:07:21 +0900

    strengthen security

--- src/components/Mainpage/Game.js
+++ src/components/Mainpage/Game.js
@@ -3,7 +3,7 @@ import './Chat.css'
 export default function Game ({isGame}) {
   return isGame && (
 <div className="chatDiv">
-      <object aria-labelledby="label1" data="http://ec2.shigojago.icugo.icu:4000/game" width="1800px" height="830px" style={{ "position":"fixed", "top":"200px", "left": "30px" }}></object>
+      <object aria-labelledby="label1" data={process.env.REACT_APP_URL+'game'} width="1800px" height="830px" style={{ "position":"fixed", "top":"200px", "left": "30px" }}></object>
     </div>
   )  
 }
